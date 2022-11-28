3a454d5a45ef12fdd5f044f7b9106297f3ab7ccc
Jefferj <jefferj080@gmail.com>
2022-11-28 16:03:22 -0500

    CompletedPageViewControllerSucefull

--- YoutubeClone/Main/MainViewController.swift
+++ YoutubeClone/Main/MainViewController.swift
@@ -8,9 +8,23 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    var rootPageViewController : RootPageViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? RootPageViewController {
+            destination.delegateRoot = self
+            rootPageViewController = destination
+        }
+    }
+}
 
+extension MainViewController : RootPageProtocol {
+    func currentPage(_ index: Int) {
+        print("currentPage: ", index)
     }
 }
