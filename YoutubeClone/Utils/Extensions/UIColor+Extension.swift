43322250ccb5d49a141fc8c45a05649cee623973
Jefferj <jefferj080@gmail.com>
2022-12-06 18:22:31 -0500

    createdTabBarButtons

---
+++ YoutubeClone/Utils/Extensions/UIColor+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor+Extension.swift
+//  YoutubeClone
+//
+//  Created by Jefferson Naranjo rodríguez on 6/12/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    @nonobjc class var backgroundColor: UIColor {
+        return UIColor(named: "backgroundColor")!
+    }
+    
+    @nonobjc class var grayColor: UIColor {
+        return UIColor(named: "grayColor")!
+    }
+    
+    @nonobjc class var whiteColor: UIColor {
+        return UIColor(named: "whiteColor")!
+    }
+}
--- YoutubeClone/Videos/View/VideosViewController.swift
+++ YoutubeClone/Videos/View/VideosViewController.swift
@@ -10,60 +10,55 @@ import UIKit
 class VideosViewController: UIViewController {
 
     @IBOutlet weak var tableViewVideos: UITableView!
-
-        lazy var presenter = VideosPresenter(delegate: self)
-        var videoList : [VideoModel.Item] = []
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            configTableView()
-            Task{
-                await presenter.getVideos()
-            }
-        }
-        
-        func configTableView(){
-            let nibVideos = UINib(nibName: "\(VideoCell.self)", bundle: nil)
-            tableViewVideos.register(nibVideos, forCellReuseIdentifier: "\(VideoCell.self)")
-            
-            tableViewVideos.separatorColor = .clear
-            tableViewVideos.delegate = self
-            tableViewVideos.dataSource = self
-        }
-
-    }
-    extension VideosViewController : UITableViewDelegate, UITableViewDataSource{
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return self.videoList.count
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let video = videoList[indexPath.row]
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(VideoCell.self)", for: indexPath) as? VideoCell else{
-                return UITableViewCell()
-            }
-            cell.didTapDostsButton = {[weak self] in
-                self?.configButtonSheet()
-            }
-            cell.configCell(model: video)
-            return cell
-        }
-        
-        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-            return 95.0
-        }
-        
-        func configButtonSheet(){
-            let vc = BottonSheetViewController()
-            vc.modalPresentationStyle = .overCurrentContext
-            self.present(vc, animated: false)
-        }
-        
-    }
-
-    extension VideosViewController : VideosViewProtocol{
-        func getVideos(videoList: [VideoModel.Item]) {
-            self.videoList = videoList
-            tableViewVideos.reloadData()
+    lazy var presenter = VideosPresenter(delegate: self)
+    var videoList : [VideoModel.Item] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configTableView()
+        Task{
+            await presenter.getVideos()
         }
     }
+    
+    func configTableView(){
+        tableViewVideos.register(cell: VideoCell.self)
+        tableViewVideos.separatorColor = .clear
+        tableViewVideos.delegate = self
+        tableViewVideos.dataSource = self
+    }
+
+}
+extension VideosViewController : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.videoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let video = videoList[indexPath.row]
+        let cell = tableView.dequeueReusableCell(for: VideoCell.self, for: indexPath)
+        cell.didTapDostsButton = {[weak self] in
+            self?.configButtonSheet()
+        }
+        cell.configCell(model: video)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 95.0
+    }
+    
+    func configButtonSheet(){
+        let vc = BottonSheetViewController()
+        vc.modalPresentationStyle = .overCurrentContext
+        self.present(vc, animated: false)
+    }
+    
+}
+
+extension VideosViewController : VideosViewProtocol{
+    func getVideos(videoList: [VideoModel.Item]) {
+        self.videoList = videoList
+        tableViewVideos.reloadData()
+    }
+}
